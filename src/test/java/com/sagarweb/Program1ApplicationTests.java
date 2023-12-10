694b71605e6a7a2d24c273b0931140764f5b6a98
DaviSales22 <davissales2010@gmail.com>
2023-12-10 09:16:24 -0300

    H2 database, test profile, JPA

---
+++ src/test/java/com/sagarweb/Program1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagarweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Program1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
